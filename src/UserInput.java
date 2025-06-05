34c66d78b890576001e40ba6fff8e1580779088e
Poovaraahan <poovaraahan@gmail.com>
2025-06-05 15:58:12 +0530

    Integer input

--- src/UserInput.java
+++ src/UserInput.java
@@ -5,6 +5,9 @@ public class UserInput {
         System.out.print ("What color is your bugatti? ");
         String color = input.nextLine();
         System.out.print(""+ color +"? That's sus...\n");
+        System.out.print("How old are you? ");
+        int age = input.nextInt();
+        System.out.print("Wow, " + age + " years old? That's pretty young!\n");
     }
 
     
