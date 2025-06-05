8695b9c62b92a619ebffd022e20588729c80dbb4
Poovaraahan <poovaraahan@gmail.com>
2025-06-05 14:22:20 +0530

    Datatype: float and double

--- src/Variables.java
+++ src/Variables.java
@@ -1,9 +1,8 @@
 public class Variables {
     public static void main(String[] args){
-        byte b = 100; // 1 byte. range = -128 to 127
-        short s = 10000; // 2 bytes. range = -32,768 to 32,767
-        System.out.println("Byte value: " + b);
-        System.out.println("Short value: " + s);
-   
+       float f = 3.14159129f; // float variable: prints up to 7 decimal places. f necessary
+       System.out.println("Float value: " + f); // Print float value
+       double d = 3.1415926535897937; // double variable: prints up to 15 decimal places
+       System.out.println("Double value: " + d); // Print double value
     }
 }
