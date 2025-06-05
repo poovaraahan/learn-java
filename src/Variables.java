3a531a1df9131d3fda060b7d1abffb3412da49b0
Poovaraahan <poovaraahan@gmail.com>
2025-06-05 14:09:35 +0530

    Datatype: Integer

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args){
+        int x; // Declaration
+        x = 10; // Assignment
+        int y = 20; // Initialization = Declaration + Assignment
+        System.out.println("Integer value: " + x +y);//function to print variable
+    }
+}
