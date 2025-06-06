f877042b9f831eb192c7c76094efbabcd59e214c
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 08:18:26 +0530

    Division operator

--- src/Expressions.java
+++ src/Expressions.java
@@ -4,8 +4,10 @@ public class Expressions {
         //values, variables, and constants are operands
         //operators : // +, -, *, /, %, ++, --, ==, !=, >, <, >=, <=, &&, ||, !
         int friends = 10; //int variable
-        friends = friends*2; //multiply by 2
+        friends = friends/3; //division operator
         System.out.println("I have " + friends + " friends!");
+        //note that the division operator will return the nearest lesser integer value
+        //Example: 3.90 will return 3, not 4
 
    
     }
