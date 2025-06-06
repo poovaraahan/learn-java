74e591116bdd61c6dd9726d0d762f60724641b80
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 08:13:04 +0530

    Subtraction operator

--- src/Expressions.java
+++ src/Expressions.java
@@ -4,9 +4,10 @@ public class Expressions {
         //values, variables, and constants are operands
         //operators : // +, -, *, /, %, ++, --, ==, !=, >, <, >=, <=, &&, ||, !
         int friends = 10; //int variable
-        friends++; //incrementing the variable by 1
+        friends = friends - 1; //subtracting 1 from the variable
         System.out.println("I have " + friends + " friends!"); 
-        friends += 5; //adding 5 to the variable
+        friends --; //decrementing the variable by 1
         System.out.println("Now I have " + friends + " friends!");
+        //note that the variable is updated in each line. 
     }
 }
