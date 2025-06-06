20caffacd6fc80e090336b8df8f857014eda58f7
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 10:20:22 +0530

    Min function

--- src/Maths.java
+++ src/Maths.java
@@ -2,7 +2,7 @@ public class Maths {
     public static void main(String[] args) {
         double x = -10;
         double y = 3.14;
-        double z = Math.max(x,y);
-        System.out.println("The maximum of " + x + " and " + y + " is: " + z);
+        double z = Math.min(x,y);
+        System.out.println("The minimum of " + x + " and " + y + " is: " + z);
     }
 }
