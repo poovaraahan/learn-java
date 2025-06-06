5cd4cefb6f7c75c92767a8e713b8152b9479e247
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 10:39:38 +0530

    A simple programme to calculate the hypotenuse of a triangle

--- src/Maths.java
+++ src/Maths.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Maths {
     public static void main(String[] args) {
-        double x = -10;
-        double y = 3.14;
-        double z = Math.ceil(y); // Round up to the nearest greater integer
-        System.out.println("The nearest greater integer value of y is " + z);
-        double w = Math.floor(y); // Round down to the nearest lesser integer
-        System.out.println("The nearest lesser integer value of y is " + w);
+        double a, b, c;// Declare variables for the sides of the triangle
+        Scanner side = new Scanner(System.in);// Create a Scanner object 'side' to read input
+        // Prompt the user to enter the lengths of the sides
+        System.out.println("Enter the first side of the triangle A:");  
+        a = side.nextDouble();
+        System.out.println("Enter the second side of the triangle B:");
+        b = side.nextDouble();
+        // Calculate the length of the hypotenuse using the Pythagorean theorem
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.println("The length of the hypotenuse C is: " + c);
+        side.close();
     }
 }
