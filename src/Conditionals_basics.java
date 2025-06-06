4029f5640aeb03a0b98e1ca573d3464cf9eeabca
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 11:21:53 +0530

    A simple Conditional statement programme

---
+++ src/Conditionals_basics.java
@@ -0,0 +1,13 @@
+public class Conditionals_basics {
+    public static void main(String[] args) {
+        int x = 5;
+        if(x==5){
+            System.out.println("(°o°)");
+        } else if(x>=5){
+            System.out.println("Hah, Basic!");
+        }
+        else {
+            System.out.println("baka");
+        }
+    }
+}
