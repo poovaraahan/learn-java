5edc7e76df82ce4d99fe44fd05167869e80b03e2
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 23:27:14 +0530

    Week 1 problem 2

--- src/VIT1.java
+++ src/VIT1.java
@@ -2,73 +2,87 @@ import java.util.Scanner;
 /*
  * Problem Statement
 
-Suresh, a tax consultant, assists clients to determine their tax bracket based on their annual income and potential deductions. He wants to create a program that calculates the effective annual income after applying a deduction and determines the tax bracket based on the effective income.
 
-1.	If the client is eligible for deductions (indicated by Yes/yes), subtract 1,000 from their annual income.
-2.	Based on the effective income after deductions, categorize the tax bracket as follows:
-3.	Low Income: If the effective income is 20,000 or less.
-4.	Middle Income: If the effective income is between 20,001 and 50,000, inclusive.
-5.	High Income: If the effective income exceeds 50,000.
 
-Can you guide Suresh in writing the program?
+John is a fitness trainer, and he wants to use the BMI calculator to assess the body mass index of his clients. He has a list of clients with their height (in meters) and weight (in kilograms). 
+
+
+
+John plans to write a program to quickly determine the BMI and provide a classification for each client.
+
+If BMI is less than 18.5, the program will classify it as "Underweight"
+If BMI is between 18.6 and 24.9, the program will classify it as "Normal Weight"
+If BMI is between 25.0 and 29.9, the program will classify it as "Overweight"
+If BMI is 30.0 or higher, the program will classify it as "Obese"
+
+
+Formula to calculate BMI = weight/(height*height)
+
 Input format :
-The first line of input consists of a double value, representing the annual income.
-The second line consists of a string, indicating if the client is eligible for deductions.
+The first line of input consists of a double value, representing the height of the person in meters.
+
+The second line consists of a double value, representing the weight of the person in kilograms.
+
 Output format :
-The first line of output prints a double value, representing the effective income, rounded off to two decimal places.
-The second line prints the tax bracket.
+The first line of output prints a double value, representing the BMI, rounded off to two decimal places.
+
+The second line prints the category they belong to.
+
+
 
 Refer to the sample output for formatting specifications.
+
 Code constraints :
-5000.00 ≤ annual income ≤ 200000.0
+In this scenario, the test cases fall under the following constraints:
+
+1.1 ≤ height ≤ 7.5
+
+1.1 ≤ weight ≤ 120.0
+
 Sample test cases :
 Input 1 :
-55000.75
-Yes
+1.2
+45.2
 Output 1 :
-Effective Income: 54000.75
-Tax Bracket: High Income
+BMI: 31.39
+Classification: Obese
 Input 2 :
-18500.0
-No
+5.5
+45.5
 Output 2 :
-Effective Income: 18500.00
-Tax Bracket: Low Income
+BMI: 1.50
+Classification: Underweight
 Input 3 :
-23945.56
-yes
+1.7
+68.3
 Output 3 :
-Effective Income: 22945.56
-Tax Bracket: Middle Income
+BMI: 23.63
+Classification: Normal Weight
 Input 4 :
-78965.89
-no
+1.6
+80.9
 Output 4 :
-Effective Income: 78965.89
-Tax Bracket: High Income
-
+BMI: 31.60
+Classification: Obese
  */
 public class VIT1{
-    public static void main (String[] args){
+       public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        double AnnualIncome = input.nextDouble();
-        input.nextLine();
-        String eligiblity = input.nextLine();
-        if(eligiblity.equals("Yes")||eligiblity.equals("yes")){
-            AnnualIncome = AnnualIncome - 1000;
-            System.out.println("Effective Income: " + AnnualIncome);
+        double height = input.nextDouble();
+        double weight = input.nextDouble();
+        double BMI = (weight)/(height*height);
+        System.out.printf("BMI: %.2f\n", BMI);
+        if(BMI<18.5){
+            System.out.println("Classificationn: Underweight");
+        }
+        else if(BMI>=18.6&&BMI<=24.9){
+            System.out.println("Classification: Normal Weight");
+        }
+        else if(BMI>=25.0&&BMI<=29.9){
+            System.out.println("Classification: Overweight");
         }
         else{
-            System.out.println("Effective Income: " + AnnualIncome);
-        }
-        if(AnnualIncome<=20000){
-            System.out.println("Tax Bracket: Low Income");
-        }
-        else if(AnnualIncome>=20001 && AnnualIncome<=50000){
-            System.out.println("Tax Bracket: Middle Income");
-        }
-        else{
-            System.out.println("Tax Bracket: High Income");
+            System.out.println("Classification: Obese");
         }
         input.close();
     }
