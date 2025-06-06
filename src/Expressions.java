75845e7f8c04fc8072fc5aee2618285b8b391763
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 08:15:52 +0530

    Multiplication operator

--- src/Expressions.java
+++ src/Expressions.java
@@ -4,10 +4,9 @@ public class Expressions {
         //values, variables, and constants are operands
         //operators : // +, -, *, /, %, ++, --, ==, !=, >, <, >=, <=, &&, ||, !
         int friends = 10; //int variable
-        friends = friends - 1; //subtracting 1 from the variable
-        System.out.println("I have " + friends + " friends!"); 
-        friends --; //decrementing the variable by 1
-        System.out.println("Now I have " + friends + " friends!");
-        //note that the variable is updated in each line. 
+        friends = friends*2; //multiply by 2
+        System.out.println("I have " + friends + " friends!");
+
+   
     }
 }
