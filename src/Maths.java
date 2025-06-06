2afc107d59037d62ef24c2e9654e17db4776c2d3
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 10:25:50 +0530

    sqrt function

--- src/Maths.java
+++ src/Maths.java
@@ -2,9 +2,7 @@ public class Maths {
     public static void main(String[] args) {
         double x = -10;
         double y = 3.14;
-        double z = Math.abs(x);
-        System.out.println("The absoulute value of x is " + z);
-        //Absolute of a number is the distance of the number from 0 on the number line.
-        //positive and decimal value.
+        double z = Math.sqrt(y); //Square root of y
+        System.out.println("The square root of y is " + z);
     }
 }
