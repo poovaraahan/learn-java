e316a05d96fb736cba6fdb70d9542d7daba1ab7d
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 10:31:01 +0530

    ceil and floor rounding

--- src/Maths.java
+++ src/Maths.java
@@ -2,7 +2,9 @@ public class Maths {
     public static void main(String[] args) {
         double x = -10;
         double y = 3.14;
-        double z = Math.round(y); //Square root of y
-        System.out.println("The integer value of y is " + z);//.0 at end of the number
+        double z = Math.ceil(y); // Round up to the nearest greater integer
+        System.out.println("The nearest greater integer value of y is " + z);
+        double w = Math.floor(y); // Round down to the nearest lesser integer
+        System.out.println("The nearest lesser integer value of y is " + w);
     }
 }
