cd4a3865c9f17f4e1359b3d0947981dba7e9afb2
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 10:19:32 +0530

    Math class intro: Max function

---
+++ src/Maths.java
@@ -0,0 +1,8 @@
+public class Maths {
+    public static void main(String[] args) {
+        double x = -10;
+        double y = 3.14;
+        double z = Math.max(x,y);
+        System.out.println("The maximum of " + x + " and " + y + " is: " + z);
+    }
+}
