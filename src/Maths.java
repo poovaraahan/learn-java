8b506bc35c548e76adeaa755c80fd0e69f3c142a
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 10:28:33 +0530

    round function

--- src/Maths.java
+++ src/Maths.java
@@ -2,7 +2,7 @@ public class Maths {
     public static void main(String[] args) {
         double x = -10;
         double y = 3.14;
-        double z = Math.sqrt(y); //Square root of y
-        System.out.println("The square root of y is " + z);
+        double z = Math.round(y); //Square root of y
+        System.out.println("The integer value of y is " + z);//.0 at end of the number
     }
 }
