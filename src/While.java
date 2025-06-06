2fa81542741363991ccb31259c441ae54e63dd86
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 19:49:14 +0530

    Loops: Simple do-while Loop

--- src/While.java
+++ src/While.java
@@ -3,14 +3,14 @@ public class While {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         String name = "";
-        //The while loop executes a line of code, as long as the condition is true.
-        //In this case, it will keep asking for the name until a non-blank name is entered.
-        while(name.isBlank()){//.isBlank() checks if the string is empty or contains only whitespace
+        //The do-while loop ensures that the code block is executed at least once
+        do{
             System.out.print("Enter your name: ");
             name = input.nextLine();
-        }
+        }while(name.isEmpty()); // The loop continues until the user enters a non-empty name
         System.out.println("Hello, " + name + "!");
         input.close(); 
+        // Though no difference of output in this case, there's a difference between do-while and while loops.
     }
     
 }
