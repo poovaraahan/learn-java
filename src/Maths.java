a795464166fde719f7c912284107ce99f30ede2d
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 10:23:30 +0530

    Abs function

--- src/Maths.java
+++ src/Maths.java
@@ -2,7 +2,9 @@ public class Maths {
     public static void main(String[] args) {
         double x = -10;
         double y = 3.14;
-        double z = Math.min(x,y);
-        System.out.println("The minimum of " + x + " and " + y + " is: " + z);
+        double z = Math.abs(x);
+        System.out.println("The absoulute value of x is " + z);
+        //Absolute of a number is the distance of the number from 0 on the number line.
+        //positive and decimal value.
     }
 }
