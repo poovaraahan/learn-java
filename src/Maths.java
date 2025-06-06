2562e0515f598521be183126f7f2e2d37ae94db2
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 10:44:55 +0530

    Math class summary

--- src/Maths.java
+++ src/Maths.java
@@ -14,3 +14,57 @@ public class Maths {
         side.close();
     }
 }
+/*
+public class Maths {
+    public static void main(String[] args) {
+        double x = -10;
+        double y = 3.14;
+        double z = Math.max(x,y);
+        System.out.println("The maximum of " + x + " and " + y + " is: " + z);
+    }
+}
+public class Maths {
+    public static void main(String[] args) {
+        double x = -10;
+        double y = 3.14;
+        double z = Math.min(x,y);
+        System.out.println("The minimum of " + x + " and " + y + " is: " + z);
+    }
+}
+public class Maths {
+    public static void main(String[] args) {
+        double x = -10;
+        double y = 3.14;
+        double z = Math.abs(x);
+        System.out.println("The absoulute value of x is " + z);
+        //Absolute of a number is the distance of the number from 0 on the number line.
+        //positive and decimal value.
+    }
+}
+public class Maths {
+    public static void main(String[] args) {
+        double x = -10;
+        double y = 3.14;
+        double z = Math.sqrt(y); //Square root of y
+        System.out.println("The square root of y is " + z);
+    }
+}
+public class Maths {
+    public static void main(String[] args) {
+        double x = -10;
+        double y = 3.14;
+        double z = Math.round(y); //Square root of y
+        System.out.println("The integer value of y is " + z);//.0 at end of the number
+    }
+}
+public class Maths {
+    public static void main(String[] args) {
+        double x = -10;
+        double y = 3.14;
+        double z = Math.ceil(y); // Round up to the nearest greater integer
+        System.out.println("The nearest greater integer value of y is " + z);
+        double w = Math.floor(y); // Round down to the nearest lesser integer
+        System.out.println("The nearest lesser integer value of y is " + w);
+    }
+}
+*/
