10c88fe913a4979dfd7ee4c7d84eb86e8ccfd404
Poovaraahan <poovaraahan@gmail.com>
2025-06-06 08:05:35 +0530

    Addition Operator

---
+++ src/Expressions.java
@@ -0,0 +1,10 @@
+public class Expressions {
+    public static void main(String[] args){
+        //expressions: operands + operators
+        //values, variables, and constants are operands
+        //operators : // +, -, *, /, %, ++, --, ==, !=, >, <, >=, <=, &&, ||, !
+        int friends = 10; //int variable
+        friends = friends + 1; //incrementing the variable by 1
+        System.out.println("I have " + friends + " friends!"); //concatenation of string and variable
+    }
+}
