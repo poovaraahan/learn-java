303ad11456ca66a83e67903afbcda447934759e3
Poovaraahan <poovaraahan@gmail.com>
2025-06-07 11:54:35 +0530

    Week 1 problem 8

--- src/VIT1.java
+++ src/VIT1.java
@@ -3,139 +3,78 @@ import java.util.Scanner;
 
 
 
-Sharon's favourite cricket team, "Kings XI Punjab" is participating in the IPL season. She wants to check the zodiac signs of the players based on their dates of birth. To do this, Sharon needs to use a zodiac sign chart that provides the zodiac sign for each date of birth.
+Given an integer as an input, representing the temperature in centigrade. Determine the weather conditions based on the temperature.
 
 
 
-Manish, Sharon's assistant, is assigned to write a program that will determine the zodiac sign based on the given date. 
-
-
-
-Help Manish by creating a program that takes a date of birth and outputs the corresponding zodiac sign according to the following chart:
-
-
-
+Temperature < 0, then print "Freezing weather".
+Temperature 0 - 9, then print "Very cold weather".
+Temperature 10 - 19, then print "Cold weather".
+Temperature 20 - 29, then print "Normal in temperature".
+Temperature 30 - 39, then print "It's hot".
+Temperature >= 40, then print "It's very hot".
 Input format :
-The input consists of two space-separated integers, representing the date and month.
+The input consists of a single integer T, representing the temperature.
 
 Output format :
-The output prints the astrological sign corresponding to the given date and month.
-
-If the date or month is above the valid range, print "Invalid Date/Month".
+The output prints the weather conditions based on the given temperature.
 
 
 
-Refer to the sample output for formatting specifications.
+Refer to the sample outputs for the formatting specifications.
 
 Code constraints :
-1 ≤ date ≤ 31
+In this scenario, the test cases fall under the following constraints:
 
-1 ≤ month ≤ 12
+−100 ≤ T ≤ 100
 
 Sample test cases :
 Input 1 :
-7 8
+-2
 Output 1 :
-Astrological sign for 7-8 is Leo
+Freezing weather
 Input 2 :
-18 2
+0
 Output 2 :
-Astrological sign for 18-2 is Aquarius
+Very cold weather
 Input 3 :
-35 7
+10
 Output 3 :
-Invalid Date/Month */
+Cold weather
+Input 4 :
+20
+Output 4 :
+Normal in temperature
+Input 5 :
+30
+Output 5 :
+Its hot
+Input 6 :
+44
+Output 6 :
+Its very hot */
 
 public class VIT1{public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int date, month;
-        date = input.nextInt();
-        month = input.nextInt();
-        if(date<1||date>31||month<1||month>12){
-            System.out.println("Invalid Date/Month");
+        int temp = input.nextInt(); //keep forgetting the damn brackets
+        if(temp<0){
+            System.out.println("Freezing weather");
+        }
+        else if(temp>=0 && temp<=9){
+            System.out.println("Very cold weather");
+        }
+        else if(temp>=10 && temp<=19){
+            System.out.println("Cold weather");
+        }
+        else if(temp>=20 && temp<=29){
+            System.out.println("Normal in temperature");
+        }
+        else if(temp>=30 && temp<=39){
+            System.out.println("It's hot");
         }
         else{
-            String sign = null;
-            int category = 0;//have to initialize it seems
-            if((month == 1 && date>=20) || (month == 2 && date<=18)){ //if((date>=20&&date<=18) && (month=1||month=2)) my initial wrong proposal. This condition will never be true because a number cannot be both >= 20 and <= 18 at the same time.
-                category = 1;//I keep forgetting == within conditionals
-            }
-            else if((month==2 && date>=19)||(month == 3 && date<=20)){
-                category = 2;
-            }
-            else if((month==3 && date>=21)||(month == 4 && date<=19)){
-                category = 3;
-            }
-            else if((month==4 && date>=20)||(month==5 && date<=20)){
-                category = 4;
-            }
-            else if((month==5 && date>=21)||(month==6 && date<=20)){
-                category = 5;
-            }
-            else if((month==6 && date>=21)||(month==7 && date<=22)){
-                category = 6;
-            }
-            else if((month==7 && date>=23)||(month==8 && date<=22)){
-                category = 7;
-            }
-            else if((month==8 && date>=23)||(month==9 && date<=22)){
-                category = 8;
-            }
-            else if((month==9 && date>=23)||(month==10 && date<=22)){
-                category = 9;
-            }
-            else if((month==10 && date>=23)||(month==11 && date<=21)){
-                category = 10;
-            }
-            else if((month==11 && date>=22)||(month==12 && date<=22)){
-                category = 11;
-            }
-            else{
-                category = 12;
-            }
-            switch(category){
-                case 1:
-                   sign = "Aquarius"; 
-                   break;//I have to remember break;
-                case 2:
-                    sign = "Pisces";
-                    break;
-                case 3:
-                    sign = "Aries";
-                    break;
-                case 4:
-                    sign = "Taurus";
-                    break;
-                case 5:
-                    sign = "Gemini";
-                    break;
-                case 6:
-                    sign = "Cancer";
-                    break;
-                case 7:
-                    sign = "Leo";
-                    break;
-                case 8:
-                    sign = "Virgo";
-                    break;
-                case 9:
-                    sign = "Libra";
-                    break;
-                case 10:
-                    sign = "Scorpio";
-                    break;
-                case 11:
-                    sign = "Saggitarius";
-                    break;
-                case 12:
-                    sign = "Capricorn";
-                    break;
-            }
-            System.out.printf("Astrological sign for %d-%d is %s", date, month, sign);
+            System.out.println("It's very hot");
         }
         input.close();
-        //I have to remember to close the scanner to avoid resource leaks
-        
-        
     }
 }
