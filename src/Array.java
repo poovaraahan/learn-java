991b41ea38df87a9f784a4851dfe7a40fbdeff5a
Poovaraahan <poovaraahan@gmail.com>
2025-06-19 09:24:56 +0530

    clear all elements from ArrayList

--- src/Array.java
+++ src/Array.java
@@ -11,6 +11,7 @@ public class Array {
         pokemon.add("Squirtle");
         pokemon.set(1, "Eevee"); //set() is used to replace an element at a specific index
         pokemon.remove(2); //remove() is used to delete an element at a specific index
+        pokemon.clear(); //clear() is used to remove all elements from the ArrayList
         for(int i = 0; i < pokemon.size(); i++) {//used size() instead of length
             System.out.println(pokemon.get(i));//get() is used to access elements in an ArrayList
         }
