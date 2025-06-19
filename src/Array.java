540b4c22503e236365e872cb63665b72fce0d1a2
Poovaraahan <poovaraahan@gmail.com>
2025-06-19 08:08:34 +0530

    Update value of a string within the array

--- src/Array.java
+++ src/Array.java
@@ -2,7 +2,9 @@ public class Array {
     public static void main(String[] args){
         // Create a simple array of Strings
         String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Elderberry"};
+        fruits[0] = "Pikachu"; // Change the first element
         System.out.println(fruits[0]); 
+
     }
     
 }
