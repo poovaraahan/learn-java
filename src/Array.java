aa6a7d805873b2ee555304b8c260fc477cb9c932
Poovaraahan <poovaraahan@gmail.com>
2025-06-19 08:45:42 +0530

    Print a Simple ArrayList

--- src/Array.java
+++ src/Array.java
@@ -1,31 +1,18 @@
+import java.util.ArrayList;
 public class Array {
     public static void main(String[] args){
-        //Create 2D array: Array of Arrays
-        String[][] alphabet = new String[3][3];
-        alphabet[0][0] = "A";
-        alphabet[0][1] = "B";
-        alphabet[0][2] = "C";
-        alphabet[1][0] = "D";
-        alphabet[1][1] = "E";
-        alphabet[1][2] = "F";
-        alphabet[2][0] = "G";
-        alphabet[2][1] = "H";
-        alphabet[2][2] = "I";
-        /*
-         * OR
-         * String[][] alphabet = {
-         * {"A", "B", "C"},
-         * {"D", "E", "F"},   
-         * {"G", "H", "I"}
-         */
-        //Use nested for loop to print the 2D array
-        for (int i = 0; i < alphabet.length; i++) {
-            System.out.println(); // New line before each row
-            for (int j = 0; j < alphabet[i].length; j++) {// Loop through each column in the row
-                System.out.print(alphabet[i][j] + " ");// " " for space between elements of each column
-            }//don't forget to add alphabet[i].length to the for inner loop condition
-
+        //Array list is a resizable array implementation in Java
+        //Elements can be added or removed after compilation
+        //store referece data types
+        ArrayList<String>pokemon = new ArrayList<String>();//() is used to create an object
+        pokemon.add("Pikachu");
+        pokemon.add("Bulbasaur");
+        pokemon.add("Charmander");
+        pokemon.add("Squirtle");
+        for(int i = 0; i < pokemon.size(); i++) {//used size() instead of length
+            System.out.println(pokemon.get(i));//get() is used to access elements in an ArrayList
         }
+        
     }
 }
 
