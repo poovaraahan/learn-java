b113a336a4c13321652978284cb88f7b63d68f89
Poovaraahan <poovaraahan@gmail.com>
2025-06-19 08:07:06 +0530

    Simple array of strings

---
+++ src/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args){
+        // Create a simple array of Strings
+        String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Elderberry"};
+        System.out.println(fruits[0]); 
+    }
+    
+}
