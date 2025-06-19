7380a0451789110bc54da2505b76160fa2f2633b
Poovaraahan <poovaraahan@gmail.com>
2025-06-19 09:22:51 +0530

    Remove element form ArrayList

--- src/Array.java
+++ src/Array.java
@@ -10,6 +10,7 @@ public class Array {
         pokemon.add("Charmander");
         pokemon.add("Squirtle");
         pokemon.set(1, "Eevee"); //set() is used to replace an element at a specific index
+        pokemon.remove(2); //remove() is used to delete an element at a specific index
         for(int i = 0; i < pokemon.size(); i++) {//used size() instead of length
             System.out.println(pokemon.get(i));//get() is used to access elements in an ArrayList
         }
