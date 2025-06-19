1d4ef3010dffdff8b8c4ae3f36ef90cafd9032d7
Poovaraahan <poovaraahan@gmail.com>
2025-06-19 10:44:29 +0530

    String methods: Length, Index and Characters

--- src/String_methods.java
+++ src/String_methods.java
@@ -3,16 +3,11 @@ public class String_methods {
         //String is a reference datatype that is used to store a sequence of characters.
         //can access useful methods
         String str = "Hello, World!";
-        boolean result = str.equalsIgnoreCase("hello, world!");// Compares two strings, ignoring case considerations. 
-        System.out.println(result);
-        boolean result2 = str.isEmpty();// Checks if the string is empty.
-        System.out.println(result2);
-        boolean result3 = str.contains("World");// Checks if the string contains a substring.
-        System.out.println(result3);
-        boolean result4 = str.startsWith("Hello");// Checks if the string starts with a prefix.
-        System.out.println(result4);
-        boolean result5 = str.endsWith("!");// Checks if the string ends with a suffix.
-        System.out.println(result5);
-
+        int length = str.length(); // Returns the length of the string
+        System.out.println(length);
+        char letter = str.charAt(12); // Returns the character at index 12
+        System.out.println(letter);
+        int index = str.indexOf("W"); // Returns the index of the first occurrence of "W"
+        System.out.println(index);
     }
 }
