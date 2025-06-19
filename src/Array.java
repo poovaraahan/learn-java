48dc909305372fd815f098eaf68e3e8f7a44b5f7
Poovaraahan <poovaraahan@gmail.com>
2025-06-19 08:15:19 +0530

    Akternate method Array

--- src/Array.java
+++ src/Array.java
@@ -1,10 +1,14 @@
 public class Array {
     public static void main(String[] args){
         // Create a simple array of Strings
-        String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Elderberry"};
-        fruits[0] = "Pikachu"; // Change the first element
-        System.out.println(fruits[5]);  // Attempt to access an out-of-bounds index
-
+        String[] pokemon = new String[6];
+        pokemon[0] = "Pikachu"; 
+        pokemon[1] = "Bulbasaur";
+        pokemon[2] = "Charmander";
+        pokemon[3] = "Squirtle";
+        pokemon[4] = "Jigglypuff";
+        pokemon[5] = "Meowth";
+        System.out.println(pokemon[5]);  
     }
     
 }
