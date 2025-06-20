814b56016a969837b36eb93111b0fd8dd16b2408
Poovaraahan <poovaraahan@gmail.com>
2025-06-20 08:06:29 +0530

    for-each loop for arrays

---
+++ src/for_each.java
@@ -0,0 +1,12 @@
+public class for_each {
+    public static void main(String[] args){
+            //Traverse through the elements of an array
+            //less steps, more readable
+            //less flexible
+            String[] pokemon = {"pikachu","bulbasaur","charmander", "squirtle"};//create string 
+            for(String i : pokemon){
+                System.out.println(i);
+            }//prints every element String[i]
+    }
+    
+}
