37409087ae465ed1a67603c4ddea58de9556ff93
Poovaraahan <poovaraahan@gmail.com>
2025-06-20 08:13:09 +0530

    for-each loop for ArrayList

--- src/for_each.java
+++ src/for_each.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
 public class for_each {
     public static void main(String[] args){
-            //Traverse through the elements of an array
+            //Traverse through the elements of an array or arraylist
             //less steps, more readable
-            //less flexible
-            String[] pokemon = {"pikachu","bulbasaur","charmander", "squirtle"};//create string 
+            //less flexible - also known as enhanced for loop
+            ArrayList<String> pokemon = new ArrayList<String>();
+            pokemon.add("pikachu");
+            pokemon.add("bulbasaur");
+            pokemon.add("charmander");
+            pokemon.add("squirtle");
             for(String i : pokemon){
                 System.out.println(i);
-            }//prints every element String[i]
+            }
+
     }
     
 }
