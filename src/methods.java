dbc48b5f8483f6d10412973f682684878e4aa084
Poovaraahan <poovaraahan@gmail.com>
2025-06-20 08:31:47 +0530

    Methods with Parameters

--- src/methods.java
+++ src/methods.java
@@ -2,9 +2,13 @@ public class methods {
     public static void main(String[] args){
         //A  method is a block of code executed when called within the main function
         //Basically functions like the python def function
-        hello();
+        String name = "Bonobo";
+        int age = 666;
+        hello(name, age);
+        //Variables being declared in the main function must also be declared as parameters within the methods
+        
     }
-    static void hello(){
-        System.out.println("homiesexual");
+    static void hello(String name, int age){//declare variable parameter with datatype
+        System.out.println("homiesexual " + name+". I am "+age+" years old");
     }
 }
