4c8f766f60bfb652e096094f3fd905404fe6c1bd
Poovaraahan <poovaraahan@gmail.com>
2025-06-20 08:24:13 +0530

    Simple method execution - no parameters

---
+++ src/methods.java
@@ -0,0 +1,10 @@
+public class methods {
+    public static void main(String[] args){
+        //A  method is a block of code executed when called within the main function
+        //Basically functions like the python def function
+        hello();
+    }
+    static void hello(){
+        System.out.println("homiesexual");
+    }
+}
