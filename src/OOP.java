5df98512ff0a41e8ab90f56798683aff89424a5b
Poovaraahan <poovaraahan@gmail.com>
2025-06-24 10:50:17 +0530

    Simple usecase for Constructors

--- src/OOP.java
+++ src/OOP.java
@@ -1,25 +1,26 @@
 public class OOP {
     public static void main(String[] args) {
-        // objects = attributes + methods -> basically a unique instance of the class
-        // attributes = properties/characteristics of the object
-        // methods = potential actions performed by the object.
-        Pokemon myPokemon = new Pokemon(); // created an instance for the class Pokemon
-        // To display the attributes of a class, or to execute the methods from the class-- Use object_name.attribute/method
-        System.out.println(myPokemon.name);
+        //Constructors allow different attributes to be assigned to each object that we instantiate
+        Pokemon myPokemon = new Pokemon("Pikachu","Mouse/Electric",1,6.0); // created an instance for the class Pokemon
+        // In this scenario, for constructors, values of the variable attributes declared in Main class and not imported from elsewhere 
         System.out.println(myPokemon.type);
         System.out.println(myPokemon.generation);
         System.out.println(myPokemon.weight);
-        myPokemon.thunderbolt();
-        myPokemon.electroball();
         // CREATING ANOTHER INSTANCE FOR THE SAME CLASS
-        Pokemon myPokemon2 = new Pokemon(); // created an instance for the class Pokemon 
+        Pokemon myPokemon2 = new Pokemon("Bulbasaur", "Grass/Poison",1,6.9); // created an instance for the class Pokemon 
         System.out.println(myPokemon2.name);
         System.out.println(myPokemon2.type);
         System.out.println(myPokemon2.generation);
         System.out.println(myPokemon2.weight);
-        myPokemon2.thunderbolt();
-        myPokemon2.electroball();
-        //Notice that the same block of code is executed twice as they inherit from the same blueprint or class
+        // CREATING ANOTHER INSTANCE FOR THE SAME CLASS
+        Pokemon myPokemon3 = new Pokemon("Chikorita", "Grass",2,6.4); // created an instance for the class Pokemon 
+        System.out.println(myPokemon3.name);
+        System.out.println(myPokemon3.type);
+        System.out.println(myPokemon3.generation);
+        System.out.println(myPokemon3.weight);
+        
+        //
         
     }
 }
+//Constructors allow different attributes to be assigned to each object that we instantiate
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,17 +1,20 @@
 public class Pokemon {
-    // Attributes for the class Pokemon
-    String name = "Pikachu";
-    String type = "Mouse/Electric";
-    int generation = 1;
-    double weight = 6.0;
+    // Attributes declared for the class Pokemon
+    String name;
+    String type;
+    int generation;
+    double weight;
+    
+    //CONSTRUCTOR: A method called when new instance/onject derived from class
 
-    // Methods for the class Pokemon
-    void thunderbolt() {
-        System.out.println("Pikachu, use thunderbolt!");
-        System.out.println("Pika.....CHUUUUUU!");
+    Pokemon(String name, String type, int generation, double weight){ //Parameters
+        this.name = name;
+        this.type = type;
+        this.generation = generation;
+        this.weight = weight;
     }
-    void electroball() {
-        System.out.println("Pikachu, use quick attack!");
-        System.out.println("Pi... pi.. pi.. pika..(panting)");
-    }
+    //It is the syntax to assing the values appropriated for each object
+    //this.attribute = parameter
+    //name (without this.) refers to the parameter passed to the constructor.
+    //Parameter: Temporary variable used to pass a value into a method or constructor 
 }
