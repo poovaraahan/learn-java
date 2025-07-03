c932762cdea35b4866356f6c72f2a57e7c12bccd
Poovaraahan <poovaraahan@gmail.com>
2025-07-03 09:34:36 +0530

    Learning toString: When the instance of the class is called in println function

---
+++ src/myInfo.java
@@ -0,0 +1,10 @@
+public class myInfo {
+    String name = "Poovaraahan";
+    int age = 19;
+    String country = "India";
+    String language = "Tamil";
+    int height = 170; 
+    int weight = 65;
+    
+
+}
---
+++ src/toString.java
@@ -0,0 +1,9 @@
+public class toString {
+    public static void main(String[] args) {
+        myInfo Poovaraahan = new myInfo();
+        System.out.println(Poovaraahan.toString());
+        // The toString() method is called automatically when you print an object
+        // It returns a string representation of the object - its addreess in memory by default
+    }
+    
+}
