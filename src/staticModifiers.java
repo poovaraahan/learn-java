b79d49d819a9dfeb59d51291638a986a3b361151
Poovaraahan <poovaraahan@gmail.com>
2025-07-03 11:03:42 +0530

    Static: footnotes

--- src/staticModifiers.java
+++ src/staticModifiers.java
@@ -7,6 +7,15 @@ public class staticModifiers {
         namelist student3 = new namelist("Sujan");
         namelist.displayStudentCount(); // Display the total number of students created
         //static methods can be called without creating an instance of the class
+        /*
+         * Basically, when u want to access a method or a variable without having to create a new instance of the class
+         * every single time... static methods and variables are used.
+         * U dont want to execute:
+         *  Math math = new Math();
+         * every time u want to use a method from the Math class, u can just use 
+         * Math.methodName();
+         * if the method is static, it shall be executed regardless of whether an instance of the class is created or not.
+         */
     }
 
 }
