24e79c935dfb86680d5b361d4a1bcbfb6dfe695e
Poovaraahan <poovaraahan@gmail.com>
2025-07-04 22:53:18 +0530

    Encapsulation: setVariable method

--- src/encap_car.java
+++ src/encap_car.java
@@ -6,9 +6,9 @@ public class encap_car {
 
     //Constructor:
     encap_car(String model, String color, int year){
-        this.model = model;
-        this.color = color;
-        this.year = year;
+        this.setModel(model);//Using the set methods to initialize the attributes
+        this.setColor(color);
+        this.setYear(year);
     }
     //As you know, private attributes cannot be accessed directly outside the class.
     //So, we provide public methods to access and modify them.
@@ -20,5 +20,16 @@ public class encap_car {
     }
     public int getYear() {
         return year;
+    }
+    //setVariables methods to modify the private attributes
+    //These methods allow us to change the values of the private attributes, outside the class.
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setYear(int year) {
+        this.year = year;
     }
 }
--- src/encapsulation.java
+++ src/encapsulation.java
@@ -5,6 +5,17 @@ public class encapsulation {
     //So, encapsulation is a way to restrict direct access to attributes, while providing public methods to access and modify them.
     public static void main(String[] args){
         encap_car myCar = new encap_car("BMW", "Black", 2020);
+        System.out.println("Before modification:");
+        //Using the get methods to access the attributes
+        System.out.println(myCar.getModel());
+        System.out.println(myCar.getColor());
+        System.out.println(myCar.getYear());
+        //Using the set methods to modify the attributes
+        myCar.setModel("Audi");
+        myCar.setColor("Red");
+        myCar.setYear(2021);
+        //Printing the modified attributes
+        System.out.println("After modification:");
         System.out.println(myCar.getModel());
         System.out.println(myCar.getColor());
         System.out.println(myCar.getYear());
