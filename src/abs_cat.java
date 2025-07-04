86a9815646b6de596129c7ba44f9a4fa72f4a8a9
Poovaraahan <poovaraahan@gmail.com>
2025-07-04 09:43:24 +0530

    Abstract footnote

--- src/abs_cat.java
+++ src/abs_cat.java
@@ -1,4 +1,6 @@
 public class abs_cat extends abs_animal {
+    // Concrete class that extends the abstract class abs_animal
+    // It must implement the abstract method makeSound... or will cause a compilation error
     @Override
     public void makeSound() {
         System.out.println("Meow");
