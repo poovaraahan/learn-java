c9188ccfa0ee8289cfc6c797ed72fbe3e801afd9
Poovaraahan <poovaraahan@gmail.com>
2025-07-04 23:10:20 +0530

    Object Copying: Assigning instances of a class to each other

--- src/encapsulation.java
+++ src/encapsulation.java
@@ -1,24 +1,29 @@
 public class encapsulation {
-    //Encapsulation: attributes are private, methods are public
-    //Private attributes can only be accessed through public methods
-    //This is a way to protect the data and hide the implementation details
-    //So, encapsulation is a way to restrict direct access to attributes, while providing public methods to access and modify them.
-    public static void main(String[] args){
+    public static void main(String[] args) {
         encap_car myCar = new encap_car("BMW", "Black", 2020);
-        System.out.println("Before modification:");
-        //Using the get methods to access the attributes
+        encap_car myCar2 = new encap_car("Mercedes", "White", 2022);
+        //Object copying: Directly assigning one object to another
+        myCar2 = myCar;
+        //Print Addresses of the objects
+        System.out.println(myCar);
+        System.out.println(myCar2+"\n");
+        //Object copying: Directly assigning one object to another
+        myCar2 = myCar;
+        //Printing the attributes of myCar using the get methods
         System.out.println(myCar.getModel());
         System.out.println(myCar.getColor());
-        System.out.println(myCar.getYear());
-        //Using the set methods to modify the attributes
-        myCar.setModel("Audi");
-        myCar.setColor("Red");
-        myCar.setYear(2021);
-        //Printing the modified attributes
-        System.out.println("After modification:");
-        System.out.println(myCar.getModel());
-        System.out.println(myCar.getColor());
-        System.out.println(myCar.getYear());
+        System.out.println(myCar.getYear() + "\n");
+        //Printing the attributes of myCar2 using the get methods
+        System.out.println(myCar2.getModel());
+        System.out.println(myCar2.getColor());
+        System.out.println(myCar2.getYear() + "\n");
+        /*
+         * In this example, equating myCar2 to myCar does not create a new object.
+         * Instead, it makes myCar2 refer to the same object as myCar.
+         * The addresses of both objects will be the same.
+         */
+
+        
     }
 
 }
