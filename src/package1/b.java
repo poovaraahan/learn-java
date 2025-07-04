417e500dc08608bf7a928fc45605c3ac3041896d
Poovaraahan <poovaraahan@gmail.com>
2025-07-04 10:51:17 +0530

    Access Modifiers: Private access modifiers

--- src/package1/b.java
+++ src/package1/b.java
@@ -1,10 +1,5 @@
 package package1;
 import package2.*;
 public class b{
-    public static void main(String[] args){
-        a newA = new a();
-        System.out.println(newA.protectedMessage);
-        //No error here because protectedMessage is accessible within the same package
-    }
-
+    private String privateMessage = "Private Access Modifier"; // This variable is accessible only within this class
 }
--- src/package2/asub.java
+++ src/package2/asub.java
@@ -4,7 +4,12 @@ public class asub extends a{
     public static void main(String[] args){
         asub newAsub = new asub();
         System.out.println(newAsub.protectedMessage);
-        //No error here because protectedMessage is accessible for subclasses
-    }
+        //No error here because protectedMessage is accessible within the same package and subclasses
+        c newC = new c();//Creating an instance of class c from package2
+        //Accessing public and default messages from class c
+        System.out.println(newC.publicMessage);
+        System.out.println(newC.defaultMessage);
+  }
+
 }
 
